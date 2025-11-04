7c4d041bc03ffc9772623e3ac531398f3bc90843
mossad <mossadmohamed6070@gmail.com>
2025-11-04 14:29:07 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,71 @@
+let btn = document.getElementById("btn");
+let inp = document.getElementById("inp");
+let boxs = document.querySelectorAll('.box');//select all divs have class 'box'
+let drag = null;
+let trash= document.getElementsByClassName("trash");
+
+btn.onclick= function(){
+    if(inp.value/*بجيب ال value الي هتتكتب في ال input*/ !='' ){
+        boxs[0].innerHTML+=`
+        <p class="item" draggable="true">${inp.value}</p>`
+        inp.value='';
+    }
+    dragItem();
+}
+function dragItem(){
+    let items = document.querySelectorAll('.item');
+    items.forEach(item=>{
+        item.addEventListener('dragstart', function(){
+            drag = item;
+            item.style.opacity = '0.5';
+        })
+        item.addEventListener('dragend',function(){
+            drag = item;
+            item.style.opacity = '1';
+        })
+
+        boxs.forEach(box=>{
+            box.addEventListener('dragover', function(e){
+                e.preventDefault();
+                this.style.background = '#090';
+                this.style.color = '#fff';
+            })
+
+            box.addEventListener('dragleave', function(){
+                this.style.background = '#fff';
+                this.style.color = '#000';
+            })
+
+            box.addEventListener('drop', function(){
+                box.append(drag);
+                this.style.background = '#fff';
+                this.style.color = '#000';
+            })
+        })
+        // trash.forEach(trash=>{
+        //     trash.addEventListener('dragover',function(){
+        //         t.preventDefault();
+        //         this.style.background = '#090';
+        //         this.style.color = '#fff';
+        //         console.log("شغال");
+        //     })
+        // })
+
+
+    })
+}
+function deleteContent(){
+    
+    trash.addEventListener('dragover',function(){
+        // t.preventDefault();
+        // this.style.background = '#090';
+        // this.style.color = '#fff';
+        console.log("true");
+    })
+    trash.addEventListener('drop', function(){
+        drag.remove();
+        this.style.background = '#fff';
+        this.style.color = '#000';
+    })
+    
+}
